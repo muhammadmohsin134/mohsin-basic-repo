e006315464093d34fba5f2765deda0a78035a43e
mohsin <mohsinedu541@gmail.com>
2025-01-22 02:41:28 +0500

    1st maximum code

---
+++ maximum/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a b;
+    cout<<"enter two integer">>endl;
+    cin>>a>>endl>>b;
+    if (a>b){
+        cout<<"maximum number is">>a;
+    }else{
+        cout<<"maximum number is">>b;
+    }
+}
