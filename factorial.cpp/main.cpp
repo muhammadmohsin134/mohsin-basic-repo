97dfdc1c516cac8e2d490bb9e660cae53ff4dd93
mohsin <mohsinedu541@gmail.com>
2025-01-22 15:08:53 +0500

    f

--- factorial.cpp/main.cpp
+++ factorial.cpp/main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+     int n;
+    long f = 1.0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Error! invalid number";
+    else {
+        for(int i = 1; i <= n; ++i) {
+            f *= i;
+        }
+        cout << "Factorial of " << n << " = " << f;
+    }
+
     return 0;
 }
